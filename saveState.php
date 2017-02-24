282c363680781442e258cf50960aa009fd273b51
aalhad.k <aalhad.k@media.net>
2017-02-24 21:14:25 +0530

    Testing save state

--- saveState.php
+++ saveState.php
@@ -6,12 +6,22 @@
  * Time: 8:08 PM
  */
 
+ini_set("display_errors", 1);
+
 require_once "functions.php";
 
-$round = safeReturn($_REQUEST, "reset");
+$round = safeReturn($_REQUEST, "round");
 $auctionStateJson = safeReturn($_REQUEST, "state");
 
-$fileName = getAuctionStateFile($round);
-file_put_contents($fileName, $auctionStateJson);
+if(isStringSet($round) && isStringSet($auctionStateJson))
+{
+    $fileName = getAuctionStateFile($round);
+    file_put_contents($fileName, $auctionStateJson);
 
-echo "1";
+    echo $fileName;
+    echo "<br>1";
+}
+else
+{
+    echo "0";
+}
