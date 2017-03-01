ac50970e32fd778639842d55f1ad02250e603d03
aalhad.k <aalhad.k@media.net>
2017-03-01 17:26:34 +0530

    Admin login

--- index.php
+++ index.php
@@ -6,4 +6,34 @@
  * Time: 1:04 AM
  */
 
-echo file_get_contents("html/home.html");
+include_once "functions.php";
+
+$admins = array
+(
+    "aalhadkulkarni" => "aiag#2112A",
+    "yogran" => "fantasy@123",
+    "guruji" => "sports@123"
+);
+
+if(isStringSet(safeReturn($_COOKIE, "auction_admin")))
+{
+    echo file_get_contents("html/home.html");
+}
+else if(isStringSet(safeReturn($_REQUEST, "adminId")))
+{
+    $adminId = safeReturn($_REQUEST, "adminId");
+    $password = safeReturn($_REQUEST, "password");
+    if(key_exists($adminId, $admins) && $password == safeReturn($admins, $adminId))
+    {
+        setcookie("auction_admin", $adminId);
+        echo file_get_contents("html/home.html");
+    }
+    else
+    {
+        echo file_get_contents("html/relogin.html");
+    }
+}
+else
+{
+    echo file_get_contents("html/login.html");
+}
