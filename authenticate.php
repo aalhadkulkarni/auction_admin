340c8f3fc284779ae7bc987c25167a84238afa8d
aalhad.k <aalhad.k@media.net>
2021-03-20 12:18:12 +0530

    live creds

--- authenticate.php
+++ authenticate.php
@@ -10,27 +10,27 @@ ini_set("display_errors", "0");
 
 $loggedInUser = null;
 
-//$users = array
-//(
-//    "admin" => "admin123",
-//    "Thane" => "2139",
-//    "Miraj" => "2859",
-//    "Karad" => "2105",
-//    "Kolhapur" => "1401",
-//    "Pune" => "2101",
-//    "Viewer" => ""
-//);
-
 $users = array
 (
     "admin" => "admin123",
-    "Thane" => "Thane",
-    "Miraj" => "Miraj",
-    "Karad" => "Karad",
-    "Kolhapur" => "Kolhapur",
-    "Pune" => "Pune",
+    "Thane" => "2139",
+    "Miraj" => "2859",
+    "Karad" => "2105",
+    "Kolhapur" => "1401",
+    "Pune" => "2101",
     "Viewer" => ""
 );
+
+//$users = array
+//(
+//    "admin" => "admin123",
+//    "Thane" => "Thane",
+//    "Miraj" => "Miraj",
+//    "Karad" => "Karad",
+//    "Kolhapur" => "Kolhapur",
+//    "Pune" => "Pune",
+//    "Viewer" => ""
+//);
 $userCookie = $_COOKIE["usercookie"];
 $logout = $_REQUEST["logout"];
 if ($logout == 1)
