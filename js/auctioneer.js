74f7a7ab9a82355ed4af0c81d39dc776f6c45638
aalhad.k <aalhad.k@media.net>
2021-03-20 06:51:04 +0530

    auctioneer js

--- js/auctioneer.js
+++ js/auctioneer.js
@@ -1,5 +1,6 @@
 (function (window, document, undefined) {
 
+    var messages = [];
     var database;
     var firebaseScript = document.createElement("script");
     firebaseScript.src = "https://www.gstatic.com/firebasejs/4.12.1/firebase.js";
@@ -14,9 +15,18 @@
         firebase.initializeApp(config);
         database = firebase.database();
 
-        init();
+        checkDiv();
     };
     document.body.appendChild(firebaseScript);
+
+    function checkDiv() {
+        var divs = document.getElementsByClassName("_1JAUF");
+        if (divs.length != 2) {
+            setTimeout(checkDiv, 50);
+            return;
+        }
+        divs[1].onclick = init;
+    }
 
     function init() {
         database.ref("auction/auctioneer/currentBid").on("value", newBid);
@@ -40,18 +50,27 @@
 
     function biddingEnded(data) {
         var text = data.val();
+        if (text == null || text == "") {
+            return;
+        }
         sendToWhatsapp("🔨");
         sendToWhatsapp(text + " 🔨");
     }
 
     function nextPlayerSelected(data) {
         var text = data.val();
+        if (text == null || text == "") {
+            return;
+        }
         sendToWhatsapp("*Starting bidding for next player*");
         sendToWhatsapp(text);
     }
 
     function summaryUpdated(data) {
         var summary = data.val();
+        if (summary == null || summary == "") {
+            return;
+        }
         summary = summary.replaceAll("<br>", "\n");
         sendToWhatsapp(summary);
     }
@@ -65,6 +84,7 @@
             document.execCommand("insertText", false, message);
             document.getElementsByClassName('_1E0Oz')[0].click();
         } catch (e) {
+            messages.push(message);
             alert("Please click on message box");
         }
     }
